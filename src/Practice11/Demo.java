f33f14290f1f1cc2550bb10f3c9775943d674b5c
adeshph <adesh.phadatare0051@gmail.com>
2023-12-19 10:48:02 +0530

    Old code storage for practice

---
+++ src/Practice11/Demo.java
@@ -0,0 +1,16 @@
+package Practice11;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+         // open the browser and navigate to a URL
+        driver.get("https://www.flipkart.com");
+	}
+
+}
